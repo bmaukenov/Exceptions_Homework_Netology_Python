e4bb8aef99fd27ae917fe554ba557de680b312e9
Beksultan Maukenov <bmaukenov@gmail.com>
2020-12-03 02:04:18 +0600

    Create 1&2_problems.py
    Решение 1 и 2 задачи в домашней работе по исключениям.

---
+++ 1&2_problems.py
@@ -0,0 +1,40 @@
+equation = input("What is your equation?").split()
+
+
+
+if equation[0] != "+" or  equation[0] != "-" or  equation[0] != "*" or  equation[0] != "/":
+  sign_correct = False
+
+try:
+ assert sign_correct
+ 
+
+
+ if equation[0] == "+":
+   answer = int(equation[1]) + int(equation[2])
+ elif equation[0] == "-":
+   answer = int(equation[1]) - int(equation[2])
+ elif equation[0] == "*":
+   answer = int(equation[1]) * int(equation[2])
+ else:
+   answer = int(equation[1]) / int(equation[2])
+ 
+
+
+except ZeroDivisionError:
+  print("Вы произвели деление на ноль, что является математической ошибкой. Попробуйте еще раз.")
+
+except AssertionError:
+  print("Вы допустили ошибку. В данной программе можно использовать только операции сложения, вычитания, умножения и деления. Попробуйте еще раз")
+
+except IndexError:
+  print("Вы ввели недостаточное количество аргументов для математической операции. Попробуйте еще раз.")
+
+except ValueError:
+  print("Вы ввели не число. Попробуйте еще раз.")
+  
+
+else:
+  print("Ответ в данном примере: ", answer)
+
+
