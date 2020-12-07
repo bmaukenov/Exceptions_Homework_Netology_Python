766e1bfdb00351fa6c2e8c53669f211957458829
Beksultan Maukenov <bmaukenov@gmail.com>
2020-12-08 02:52:29 +0600

    Update 1&2_problems.py

--- 1&2_problems.py
+++ 1&2_problems.py
@@ -2,39 +2,37 @@ equation = input("What is your equation?").split()
 
 
 
-if equation[0] == "+" or equation[0] == "-" or equation[0] == "*" or equation[0] == "/":
-  sign_correct = False
 
 try:
-  assert sign_correct
+    assert equation[0] in ['+', '-', '*', '/']
  
 
 
-  if equation[0] == "+":
-    answer = int(equation[1]) + int(equation[2])
-  elif equation[0] == "-":
-    answer = int(equation[1]) - int(equation[2])
-  elif equation[0] == "*":
-    answer = int(equation[1]) * int(equation[2])
-  else:
-    answer = int(equation[1]) / int(equation[2])
+    if equation[0] == "+":
+        answer = int(equation[1]) + int(equation[2])
+    elif equation[0] == "-":
+        answer = int(equation[1]) - int(equation[2])
+    elif equation[0] == "*":
+        answer = int(equation[1]) * int(equation[2])
+    else:
+        answer = int(equation[1]) / int(equation[2])
  
 
 
 except ZeroDivisionError:
-  print("Вы произвели деление на ноль, что является математической ошибкой. Попробуйте еще раз.")
+    print("Вы произвели деление на ноль, что является математической ошибкой. Попробуйте еще раз.")
 
 except AssertionError:
-  print("Вы допустили ошибку. В данной программе можно использовать только операции сложения, вычитания, умножения и деления. Попробуйте еще раз")
+    print("Вы допустили ошибку. В данной программе можно использовать только операции сложения, вычитания, умножения и деления. Попробуйте еще раз")
 
 except IndexError:
-  print("Вы ввели недостаточное количество аргументов для математической операции. Попробуйте еще раз.")
+    print("Вы ввели недостаточное количество аргументов для математической операции. Попробуйте еще раз.")
 
 except ValueError:
-  print("Вы ввели не число. Попробуйте еще раз.")
+    print("Вы ввели не число. Попробуйте еще раз.")
   
 
 else:
-  print("Ответ в данном примере: ", answer)
+    print("Ответ в данном примере: ", answer)
 
 
